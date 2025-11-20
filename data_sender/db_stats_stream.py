5469e02329404106aa5f7b1f703818169f74c8a3
jmlopezrianiJave <jmlopezriani@javerianacali.edu.co>
2025-11-19 21:32:28 -0500

    Correction

--- data_sender/db_stats_stream.py
+++ data_sender/db_stats_stream.py
@@ -3,20 +3,25 @@ import pandas as pd
 from pythonosc import udp_client
 from config import OSCConfig
 
-def send_stats(client, base_addr: str, values: pd.Series) -> None:
+def send_stats(clients, base_addr: str, values: pd.Series) -> None:
     mean_val = float(values.mean())
     std_val = float(values.std())
     min_val = float(values.min())
     max_val = float(values.max())
 
-    client.send_message(f"{base_addr}/mean", mean_val)
-    client.send_message(f"{base_addr}/std", std_val)
-    client.send_message(f"{base_addr}/min", min_val)
-    client.send_message(f"{base_addr}/max", max_val)
+    for client in clients:
+        client.send_message(f"{base_addr}/mean", mean_val)
+        client.send_message(f"{base_addr}/std", std_val)
+        client.send_message(f"{base_addr}/min", min_val)
+        client.send_message(f"{base_addr}/max", max_val)
 
 def main() -> None:
     osc_conf = OSCConfig(host="127.0.0.1", port=5005)
-    client = udp_client.SimpleUDPClient(osc_conf.host, osc_conf.port)
+
+    client_pd = udp_client.SimpleUDPClient(osc_conf.host, osc_conf.port)
+    client_processing = udp_client.SimpleUDPClient(osc_conf.host, 5006)
+
+    clients = [client_pd, client_processing]
 
     df = pd.read_csv("data_sender/StudentsPerformance.csv")
 
@@ -24,13 +29,14 @@ def main() -> None:
         current = df.iloc[: i + 1]
         count = len(current)
 
-        client.send_message("/stats/count", int(count))
+        for c in clients:
+            c.send_message("/stats/count", int(count))
 
-        send_stats(client, "/stats/math", current["math score"])
-        send_stats(client, "/stats/reading", current["reading score"])
-        send_stats(client, "/stats/writing", current["writing score"])
+        send_stats(clients, "/stats/math", current["math score"])
+        send_stats(clients, "/stats/reading", current["reading score"])
+        send_stats(clients, "/stats/writing", current["writing score"])
 
-        time.sleep(3.0)
+        time.sleep(1.0)
 
 if __name__ == "__main__":
     main()
